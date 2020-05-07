d88ddd794fbfecd4253659c8b04dccbe8390dfcb
永山奈留未 <naruminagayama@nagayamanaruminoMacBook.local>
2020-05-07 15:59:48 +0900

    Start 3-9

--- sample_app/config/initializers/application_controller_renderer.rb
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '659f3a119763606da6ea53e7519866f8338c8a113fb287ada64e3c26ed2e5fd3f17fd95a528ced8f597d5b8361fd244546a1a3c962c4e2e0f91ecab149d564b2'
