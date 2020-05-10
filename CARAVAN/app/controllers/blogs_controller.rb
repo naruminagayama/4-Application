8e6754b81c0251ddaa600b5014818348d5798daa
永山奈留未 <naruminagayama@nagayamanaruminoMacBook.local>
2020-05-11 03:08:19 +0900

    Finish 4-7

--- CARAVAN/app/controllers/blogs_controller.rb
+++ CARAVAN/app/controllers/blogs_controller.rb
@@ -28,8 +28,11 @@ class BlogsController < ApplicationController
   end
 
   def destroy
+  	blog = Blog.find(params[:id])
+  	blog.destroy
+  	redirect_to blogs_path
   end
-  
+
   private
   def blog_params
   	params.require(:blog).permit(:title, :category, :body)
